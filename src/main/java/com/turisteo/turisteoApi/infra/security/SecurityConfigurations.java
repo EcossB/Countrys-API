83c7c8ce012914753797ac3b43f69c04b7fc3b6e
EcossB <erwingcoss21@hotmail.com>
2023-11-22 20:43:15 -0400

    Adding Cors configuration at the SecurityConfigurations

--- src/main/java/com/turisteo/turisteoApi/infra/security/SecurityConfigurations.java
+++ src/main/java/com/turisteo/turisteoApi/infra/security/SecurityConfigurations.java
@@ -1,11 +1,13 @@
 package com.turisteo.turisteoApi.infra.security;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +16,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +45,7 @@ public class SecurityConfigurations {
     
         return httpSecurity.csrf( csrf -> csrf.disable()).sessionManagement( sess ->
                 sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests( auth -> auth.requestMatchers(HttpMethod.POST, "/login").permitAll()
                         //Permisos para que todos los usuarios puedan utilizar el metodo Get
                         .requestMatchers(HttpMethod.POST, "/signup").permitAll()
@@ -79,6 +88,19 @@ public class SecurityConfigurations {
                 .build();
     
 }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+       // configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
 /*
