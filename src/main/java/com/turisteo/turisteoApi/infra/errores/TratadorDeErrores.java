86524d2777947b87d1bbbd4c69ceec0905eb3fb9
EcossB <erwingcoss21@hotmail.com>
2023-12-14 15:16:08 -0400

    adding advice controllerr to handle problems.

--- src/main/java/com/turisteo/turisteoApi/infra/errores/TratadorDeErrores.java
+++ src/main/java/com/turisteo/turisteoApi/infra/errores/TratadorDeErrores.java
@@ -2,11 +2,14 @@ package com.turisteo.turisteoApi.infra.errores;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ValidationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 
 @RestControllerAdvice
@@ -31,6 +34,16 @@ public class TratadorDeErrores {
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity errorHandlerValidacionesDeNegocios(Exception exception){
         return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity errorConstraintViolation(){
+        return  ResponseEntity.badRequest().body("El campo pais_id, tiene un id de un pais que no existe.");
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity errorDatosDuplicados(){
+        return ResponseEntity.badRequest().body("Este pais ya esta agregado. Si no estas guardando un pais pues l campo pais_id, tiene un id de un pais que no existe. ");
     }
 
 
