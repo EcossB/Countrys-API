b323d057eaa6af22295c755e46571909e347b758
EcossB <erwingcoss21@hotmail.com>
2023-12-14 15:15:42 -0400

    Adding a custom url for generated server url

--- src/main/java/com/turisteo/turisteoApi/infra/springdoc/SpringDocConfig.java
+++ src/main/java/com/turisteo/turisteoApi/infra/springdoc/SpringDocConfig.java
@@ -3,15 +3,18 @@ package com.turisteo.turisteoApi.infra.springdoc;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SpringDocConfig {
 
     @Bean
     public OpenAPI customOpenApi(){
-        return new OpenAPI()
+        return new OpenAPI().servers(List.of(new Server().url("https://springgcp-405619.ue.r.appspot.com")))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
